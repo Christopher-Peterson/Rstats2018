fa266514127bb6000d9756b430ce43b52bd17263
Christopher-Peterson <Christopher.R.Peterson@gmail.com>
2018-10-16 13:56:35 -0500

    Re-organized function script, introduced argument splicing, added some examples.

--- scripts/5_functions.r
+++ scripts/5_functions.r
@@ -4,51 +4,23 @@ library(cowplot) # Not sure if we need this, but why not?
 lizards = read_csv("data/anoles.csv")
 # Most things you use in R are functions.  Example:
 
-mean(1:5) # The mean function takes the arguments c(1,2,3,4,5) and returns a single value, the mean
+mean(1:5) # The mean function takes the arguments c(1,2,3,4,5) 
+        # and returns a single value, the mean
+mean(lizards$SVL)
+lizards %>% summarize(mean_SVL = mean(SVL))
 
-# You can write your own functions.
-# Functions let you avoid errors from copy/paste duplications
-# If you need to change something, you only need to modify the code in one location, which reduces errors
-# Giving a function a good name makes the code easier to read
+# How could we write our own mean function?
+mean2 <- # function name
+  function(x) { # defines a function with argument X
+    # Everything within the braces is part of the BODY of the function
+  sum(x) / length(x) # Evaluates this expression; 
+  # the last expression evaluated is returned (a.k.a., the output)
+  }
+mean2(1:5)
+mean2(lizards$SVL)
+lizards %>% summarize(mean_SVL = mean2(SVL))
 
-# For example, let's say I wanted to estimate the diversity of lizard colors at each site 
-  # using Shannon diversity: exp( - sum(prop_i * log(prop_i))), 
-  # where prop is the proportion of individuals of a given type
-
-# We could arguably include this code as part of a summarize command:
-
-lizards %>% group_by(Site, Color) %>% summarize(n_i = n()) %>% 
-  group_by(Site) %>% mutate(prop_i = n_i / sum(n_i)) %>% 
-  summarize(diversity = exp(-sum(prop_i * log(prop_i))))
-# It's not particularly clear what's happening here, though
-  # And good luck sharing this w/ a collaborator who isn't familiar with the math
-
-# Alternatively, you could define a function
-shannon_diversity = function(types) { # types is an argument of the function
-  # This is the body of a function;
-  # you can define local variables here that will disappear after the function is run.
-  counts <- table(types)  # table counts the number of occurencesof each t
-  proportions <- counts / sum(counts)
-  out <- exp(-sum(proportions * log(proportions))) 
-  out # The final value that's evalulated or printed in a function is returned
-}
-lizards %>% group_by(Site) %>% # This bit of code is much more readable 
-  summarize(diversity = shannon_diversity(Color))
-# This also makes it easy to re-use and expand
-lizards %>% group_by(Site) %>% # This bit of code is much more readable 
-  summarize(color_diversity = shannon_diversity(Color),
-            perch_diversity = shannon_diversity(Perch_type))
-
-
-
-# Exercise:
-# write a function that 
-
-
-
-
-# Here's another example:
-
+# Let's have another example:
 normalize = function(x, ...) { # We have one argument (x) and dots (...)
   # This function centers a vector around its mean and then divides it by its standard deviation
   # The function scale() does this already, but it doesn't always play nicely with mutate()
@@ -57,6 +29,7 @@ normalize = function(x, ...) { # We have one argument (x) and dots (...)
   (x - mean_val) / stdev # this is the last command evaluated, so it's returned as the output
   # I could have simplified this as (x - mean(x, ...))/sd(x, ...)
 }
+normalize(1:5)
 
 # What do dots do? They add flexibility.  For example: 
 missing_vector = c(1:5, NA) # This has a missing value in it
@@ -93,7 +66,7 @@ normalize3(missing_vector, FALSE) # You'll get weird errors if this happens
   # In some cases, it's a good idea to normalize data by dividing by the mean instead of the sd
   # Other times, people just want to center around the mean without affecting the scale
 normalize_general = 
-  function(x,method = c("sd", "mean", "center"), # this lists the possible options for method
+  function(x, method = c("sd", "mean", "center"), # this lists the possible options for method
            ..., na.rm = TRUE) {
   method = match.arg(method) # This ensures that method is one of the options defined in the arugments
   # If method isn't explicitly declared, match.arg() sets it to the first option.
@@ -104,7 +77,7 @@ normalize_general =
     out = (x - mean_val) / stdev
   } else if (method == "mean") { 
     out = x / mean_val
-  } else { # The only other option is method == "center"
+  } else if (method == "center") { # The only other option is method == "center"
     out = x - mean_val
   }
   out # return the output variable
@@ -116,7 +89,71 @@ normalize_general(missing_vector, method = "mean") # Goes with the other option
 normalize_general(missing_vector, method = "m") # match.arg() finds the best match for incomplete options
 normalize_general(missing_vector, method = "center")
 
+# A different way to pass arguments to functions
+extra_args = list(method = "mean", na.rm = FALSE)
+extra_args
+normalize_general(x = 1:5, !!!extra_args) 
+# Placing !!! before a list will splice the elements of that list into the function call as extra arguments
+normalize_general(x = 1:5, !!!extra_args)  ==  normalize_general(x = 1:5, method = "mean", na.rm = FALSE)
+
+# This is incredibly useful when you're passing arguments to specific functions;
+  # it allows a lot more flexibility than dots
+
+normalize_general_splice = 
+  function(x, method = c("sd", "mean", "center"), 
+           mean_args = list(), sd_args = list()) {
+    method = match.arg(method) 
+    mean_val = mean(x, !!!mean_args) # argument splicing
+    if(method == "sd") { 
+      stdev = sd(x, !!!sd_args) # argument splicing
+      out = (x - mean_val) / stdev
+    } else if (method == "mean") { 
+      out = x / mean_val
+    } else if (method == "center") { # The only other option is method == "center"
+      out = x - mean_val
+    }
+    out 
+  }
+normalize_general_splice(missing_vector, method = "sd", 
+                         mean_args = list(na.rm = TRUE, trim = 0.05),
+                         se_args = list(na.rm = FALSE))
+
 #### Advice on function writing #####
+
+# You can write your own functions.
+# Functions let you avoid errors from copy/paste duplications
+# If you need to change something, you only need to modify the code in one location, which reduces errors
+# Giving a function a good name makes the code easier to read
+
+# For example, let's say I wanted to estimate the diversity of lizard colors at each site 
+# using Shannon diversity: exp( - sum(prop_i * log(prop_i))), 
+# where prop is the proportion of individuals of a given type
+
+# We could arguably include this code as part of a summarize command:
+
+lizards %>% group_by(Site, Color) %>% summarize(n_i = n()) %>% 
+  group_by(Site) %>% mutate(prop_i = n_i / sum(n_i)) %>% 
+  summarize(diversity = exp(-sum(prop_i * log(prop_i))))
+# It's not particularly clear what's happening here, though
+# And good luck sharing this w/ a collaborator who isn't familiar with the math
+
+# Alternatively, you could define a function
+shannon_diversity <- function(types) { # types is an argument of the function
+  # This is the body of a function;
+  # you can define local variables here that will disappear after the function is run.
+  counts <- table(types)  # table counts the number of occurencesof each t
+  proportions <- counts / sum(counts)
+  out <- exp(-sum(proportions * log(proportions))) 
+  out # The final value that's evalulated or printed in a function is returned
+}
+lizards %>% group_by(Site) %>% # This bit of code is much more readable 
+  summarize(diversity = shannon_diversity(Color))
+# This also makes it easy to re-use and expand
+lizards %>% group_by(Site) %>% # This bit of code is much more readable 
+  summarize(color_diversity = shannon_diversity(Color),
+            perch_diversity = shannon_diversity(Perch_type))
+
+
 
 # Make your function names descriptive, but not irritatingly long
   # Generally, function names should be verbs and arguments should be nouns
@@ -136,6 +173,7 @@ args(normalize_general) # list the arguments of a function
 important_variable = -1
 
 important_function = function(x) {
+  
   important_varaible = 10
   x * important_variable
 }
@@ -144,10 +182,29 @@ important_function(1:10)
 
 # Generally, it's a good idea to try to keep your local variable names different from the ones you use outside of functions
 
+#### Let's write some functions! ####
 
+lizards = read_csv("data/anoles.csv")
 
+# Let's see how the Limb Length : Height relationship varies by site:
 
+lizards %>% 
+  # filter(Site == "A") %>% 
+  ggplot(aes(x = Limb, y = Height)) +
+  facet_wrap(~Site, scale = "free")+
+  geom_smooth(method = "lm") + geom_point()
+# This is fine, but it's a lot of information on one plot
+# What if we wanted to make separate plots for each site? 
 
+# Use a filter() command to make it work for only a single site:
+lizards %>% 
+  filter(Site == "A") %>%
+  ggplot(aes(x = Limb, y = Height)) +
+  facet_wrap(~Site, scale = "free")+
+  geom_smooth(method = "lm") + geom_point()
 
+# How could you make this into a function that could plot arbitrary sites?
+# Modify this function to allow for optional arguments to 
+  # geom_point and geom_smooth
 
 
